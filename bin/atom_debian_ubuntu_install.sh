250b9f267185fcc1b6e226a2ced9e3d12223a3f8
Luis SEVERINO <lfs.severino@gmail.com>
2017-05-03 07:28:39 +0200

    Update bin/atom_debian_ubuntu_install.sh file

--- bin/atom_debian_ubuntu_install.sh
+++ bin/atom_debian_ubuntu_install.sh
@@ -5,8 +5,8 @@ wget -q --show-progress https://atom.io/download/deb -O atom-amd64.deb
 # Install
 sudo dpkg --install atom-amd64.deb > /dev/null 2>&1
 # Install dependencies
-apm install --packages-file packages.list
+apm install --packages-file ../config/packages.list
 # Config
-cp config.cson ~/.atom/
+cp ../config/config.cson ~/.atom/
 # Run
 atom
