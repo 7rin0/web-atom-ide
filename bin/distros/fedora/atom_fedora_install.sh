b7f3cdc81f8d59a52e4201b5f3c209ed974117ca
Luis SEVERINO <lfs.severino@gmail.com>
2017-05-16 10:18:38 +0200

    Fix atom package delete command

--- bin/distros/fedora/atom_fedora_install.sh
+++ bin/distros/fedora/atom_fedora_install.sh
@@ -17,7 +17,7 @@ wget https://raw.githubusercontent.com/7rin0/config-atom/master/config/config.cs
 cp config.cson ~/.atom
 
 # Remove temporary files
-rm -rf atom-amd64.deb packages.list config.cson
+rm -rf atom.x86_64.rpm packages.list config.cson
 
 # Run
 atom
