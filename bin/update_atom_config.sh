29963fc38d39adce7d5959ad731d22a41ac5aa65
Luis SEVERINO <lfs.severino@gmail.com>
2017-05-19 11:15:46 +0200

    Add fast updater file

--- bin/update_atom_config.sh
+++ bin/update_atom_config.sh
@@ -2,9 +2,17 @@
 
 sudo ls -al
 
-# Config
+# Update Config
 wget https://raw.githubusercontent.com/7rin0/web-atom-ide/master/config/config.cson
 cp -fr config.cson ~/.atom
+
+# Update styles.
+wget https://raw.githubusercontent.com/7rin0/web-atom-ide/master/config/styles.less
+cp -fr styles.less ~/.atom
+
+# Remove previously added dependencies and flagged as deleted.
+apm remove atom-commander
+apm remove terminal-plus
 
 # Remove temporary files
 rm -rf config.cson
